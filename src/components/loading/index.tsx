8ccf153f04331770d5c91f941a4d3099c3b0f9ee
Shiva Krishna <129250916+Shivakrishna81@users.noreply.github.com>
2024-10-20 21:38:09 +0530

    'shivathings'

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,15 +1,29 @@
 import './index.css'
-//import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+declare global {
+    namespace JSX {
+      interface IntrinsicElements {
+        'dotlottie-player': React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          src?: string;
+          background?: string;
+          speed?: number | string;
+          style?: React.CSSProperties;
+          loop?: boolean;
+          autoplay?: boolean;
+        };
+      }
+    }
+  }
 
-export const Loading = () => {
-    return (
+export const Loading=()=>{
+   
+    return(
         <div className="loader-container">
-            {/* <DotLottieReact
-                src="path/to/animation.lottie"
-                loop
-                autoplay
-            /> */}
-            <h1>Hold on for a While!</h1>
+            <dotlottie-player src="https://lottie.host/120c7961-883c-4e1a-b49a-44936cea1ff0/5Gw9memTNn.json" background="transparent" speed="1" style={{ width: '500px', height: '500px' ,margin:'0px'}} loop autoplay></dotlottie-player>
+
+            <h1>Hold on for a while.</h1>
         </div>
     )
 }
